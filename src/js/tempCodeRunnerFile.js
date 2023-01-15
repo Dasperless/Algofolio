2a56c17e0085e9f2fe78fc79de1aaa41428e8363
Dasperless <dariovm13@gmail.com>
2023-01-14 23:49:12 -0600

    feat(algorithm): :sparkles: Tree script
    A binary tree class with insert node function.

---
+++ src/js/tempCodeRunnerFile.js
@@ -0,0 +1,37 @@
+class Node{
+	constructor(value){
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree{
+	constructor(){
+		this.root = null;
+	}
+
+	insert(value){
+		this.root = this.insertNode(this.root, value);
+	}
+
+	insertNode(node, value){
+		if(node === null){
+			node = new Node(value);
+		}
+		else if(value < node.value){
+			node.left = this.insertNode(node.left, value);
+		}
+		else if(value > node.value){
+			node.right = this.insertNode(node.right, value);
+		}
+		return node;
+	}
+
+}
+
+let tree = new Tree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+console.log(tree);
